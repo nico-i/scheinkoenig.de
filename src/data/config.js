dcc425f88b12d9f41fe51e3266415724f0522ccf
Nico Ismaili <nico@ismaili.de>
2022-07-28 14:25:41 +0200

    ✨ Added videourl to GraphQL Query

--- src/data/config.js
+++ src/data/config.js
@@ -3,13 +3,18 @@ import { gql } from '@apollo/client';
 export const QUERY_CONFIG = gql`
   {
     page(id: "/work/", idType: URI) {
-    config {
-            colorBg
-            colorPrimary
-            colorText
-            fontText
-            fontTheme
+      config {
+        colorBg
+        colorPrimary
+        colorText
+        fontText
+        fontTheme
+        imgPrepageBg {
+          sourceUrl
+          altText
         }
+        urlPrepageBgVideo
+      }
     }
   }
 `;
