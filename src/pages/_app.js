3f0a97df3ad1847b6732035cb83998531a678516
Nico Ismaili <nico@ismaili.de>
2022-07-29 10:46:08 +0200

    🗑️ Removed menus

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,15 @@ import NextApp from 'next/app';
 import { SiteContext, useSiteContext } from 'hooks/use-site';
 
 import { getCategories } from 'lib/categories';
-import { createMenuFromPages, getAllMenus, MENU_LOCATION_NAVIGATION_DEFAULT } from 'lib/menus';
-import { getTopLevelPages } from 'lib/pages';
 import { getRecentPosts } from 'lib/posts';
 import { getSiteMetadata } from 'lib/site';
 import 'styles/globals.css';
 
-function App({ Component, pageProps = {}, metadata, recentPosts, categories, menus }) {
+function App({ Component, pageProps = {}, metadata, recentPosts, categories }) {
   const site = useSiteContext({
     metadata,
     recentPosts,
     categories,
-    menus,
   });
   return (
     <SiteContext.Provider value={site}>
@@ -35,23 +32,11 @@ App.getInitialProps = async function (appContext) {
     count: 5,
   });
 
-  const { menus } = await getAllMenus();
-
-  const defaultNavigation = createMenuFromPages({
-    locations: [MENU_LOCATION_NAVIGATION_DEFAULT],
-    pages: await getTopLevelPages({
-      queryIncludes: 'index',
-    }),
-  });
-
-  menus.push(defaultNavigation);
-
   return {
     ...appProps,
     metadata: await getSiteMetadata(),
     recentPosts,
     categories,
-    menus,
   };
 };
 
